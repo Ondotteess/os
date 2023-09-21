253c2a7f9a4a2ef2e993371153741ff99d6051ed
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-09-21 20:00:37 +0300

    Update build.bat

--- code/1/build.bat
+++ code/1/build.bat
@@ -7,7 +7,7 @@ move ..\..\..\NASM\nasm.exe ..\..\..\bin
 :: по хорошему эти пути вынести в переменные выглядеть это будет так
 :: set /p PATH=enter the path:
 :: echo your path: %PATH%
-:: но это заебаться можно каждый запуск 
+:: но это заебаться можно каждый запуск прописывать
 
 
 ..\..\..\bin\bash --login -c 'cd ..; cd code; cd 1; nasm -fbin boot.asm -o boot.bin; dd if=/dev/zero of=boot.img bs=1024 count=1440; dd if=boot.bin of=boot.img conv=notrunc; bochs -f os.bxrc -q; rm boot.bin boot.img'
