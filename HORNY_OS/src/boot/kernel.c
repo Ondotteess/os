f1ffb9bac91214edfc74bcc575016710f3fae9e6
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-11-03 04:15:09 +0700

    Add files via upload

--- HORNY_OS/src/boot/kernel.c
+++ HORNY_OS/src/boot/kernel.c
@@ -14,6 +14,9 @@
 #define LENGTH 80
 #define HEIGHT 25
 
+int X;
+int Y;
+
 
 void vga_clear_screen(){
 	short int* start = (short int*) START;
@@ -23,9 +26,11 @@ void vga_clear_screen(){
 }
 
 void vga_print_char(char symbol, int x, int y){
-	short int bit_mask = 0b0000101000000000;
-	bit_mask |= (short int) symbol;
-	*((short int*) START + (y * 80 + x)) = bit_mask;  
+	if (symbol != '\n') {
+		short int bit_mask = 0b0000101000000000;
+		bit_mask |= (short int) symbol;
+		*((short int*) START + (y * LENGTH + x)) = bit_mask;  
+	}
 }
 
 void vga_test_fill_screen(char symbol){
@@ -35,11 +40,172 @@ void vga_test_fill_screen(char symbol){
 		}
 	}
 }
-void vga_print_str(char* str, int x, int y){}
+
+
+void scroll() {
+	short int* start = (short int*) START;
+	for (int i = 0; i < SIZE - LENGTH; i++){
+		*((short int*) (start + i)) = *((short int*) (start + i + LENGTH));
+	}
+	Y = HEIGHT - 1;
+}
+
+
+void vga_print_str(char* str, int x, int y){
+	int index = 0;
+	int x_coord = x;
+	int y_coord = y;
+
+	while (str[index] != '\0'){
+		char curr = str[index++];
+		if (curr == '\n') {
+			x_coord = LENGTH;
+		}
+		vga_print_char(curr, x_coord, y_coord);
+		x_coord++;
+		if (x_coord >= LENGTH){
+			y_coord++;
+			x_coord = 0;
+		}
+	}
+}
+
+void init_printer(){
+	X = 0;
+	Y = 0;
+	vga_clear_screen();
+}
+
+int len(char* s) {
+	int i = 0;
+	while (*(s + i++) != '\0');
+	return i;
+}
+
+void scroll_check(int i) {
+	int y = Y + (X + i) / LENGTH;
+	if (y >= HEIGHT) {
+		int diff = y - HEIGHT + 1;
+		for (int i = 0; i < diff; i++) {
+			scroll();
+		}
+		Y -= diff;
+	}
+}
+
+
+int power(int x, int y) {
+    int res = 1;
+	if (y == 0) {
+		return 1;
+	}
+	for (int i = 0; i < y; i++) {
+		res *= x;
+	}
+	return res;
+}
+
+int numLen(int x, int base) {
+	int c = x;
+	int len = 0;
+	while (c > 0) {
+		c /= base;
+		len++;
+	}
+	return len;
+}
+
+
+void printNum(int x, int len, int base) {
+	len--;
+	if (base == 10) {
+		if (x < 0) {
+			scroll_check(1);
+			vga_print_char('-', X++, Y);
+			x *= -1;
+		}
+	}
+	while (len >= 0) {
+		int p = power(base, len--);
+		int curr = x / p;
+		char c_curr;
+		if (curr < 10) {
+			c_curr = '0' + curr;
+		} else {
+			c_curr = 'A' + curr - 10;
+		}
+		vga_print_char(c_curr, X++, Y);
+		x %= p;
+	}
+}
+
+void print(char* fmt, ...){
+	char* pointer = (void*) &fmt;
+	pointer += sizeof(char*);
+
+	while (*fmt != '\0'){
+		char curr = *fmt;
+
+		if (curr == '%') {
+			int l;
+			int n;
+			switch (*++fmt)
+			{
+			case 's':
+				char* s = *((char**) pointer);
+				pointer += sizeof(char*);
+				l = len(s);
+				scroll_check(l);
+				vga_print_str(s, X, Y);
+				X = (X + l - 1) % LENGTH;
+				break;
+			case 'd':
+				n = *((int*) pointer);
+				pointer += sizeof(int);
+				l = numLen(n, 10);
+				scroll_check(l);
+				printNum(n, l, 10);
+				break;
+			case 'x':
+				n = *((int*) pointer);
+				pointer += sizeof(int);
+				l = numLen(n, 16);
+				scroll_check(l);
+				printNum(n, l, 16);
+				break;
+
+			default:
+				break;
+			}
+		} else {
+			if (curr == '\n') {
+				scroll_check(LENGTH);
+				Y++;
+				X = 0;
+			} else { 
+				scroll_check(1);
+				vga_print_char(curr, X++, Y);
+			}
+		}
+		fmt++;
+	}
+}
 
 void __main(){
-	vga_clear_screen();	
-	vga_print_char('A', 10, 10);
+	init_printer();
+	char* s =
+"\n\n\n\n\n\n\n \
+                                    _______________ \n\
+                                    |              | \n\
+                                    |   H O R N Y  | \n\
+                            /\\      |______________|\n\
+                           | |   ( \\_/ )   ||       \n\
+                           | |   (^ 3 ^)   ||        \n\
+                          _| |_  /     \\   ||       \n\
+                            q===|       |===p        \n\
+                                 \\_ o _/            \n\
+                                  ^   ^";
+	print(s);
 	for (;;);
 }
 
